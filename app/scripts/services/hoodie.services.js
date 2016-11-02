69e8020ceca343c7ce3757b0c31a77044fd0efd1
Alexis Launay <yorunohikage@gmail.com>
2016-11-02 18:05:53 +0100

    Add coupons backend validation

--- app/scripts/services/hoodie.services.js
+++ app/scripts/services/hoodie.services.js
@@ -132,6 +132,10 @@ export default class HoodieApi {
 		});
 	}
 
+	static validateCoupon({coupon, plan}) {
+		return fetchAWS(`/coupons/${coupon}?plan=${plan}`);
+	}
+
 	static updateSubscription(options) {
 		const {subscriptionId} = HoodieApi.instance;
 
